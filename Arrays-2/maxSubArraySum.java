c28878d130eb14f78de2528512dc48bbf4c97441
Manoj Dhage <manojdhage1341@gmail.com>
2025-08-07 22:04:24 +0530

    max sub array sum

--- Arrays-2/maxSubArraySum.java
+++ Arrays-2/maxSubArraySum.java
@@ -1,6 +1,31 @@
 public class maxSubArraySum {
-    public static void main(String[] args) {
+
+    public static void maxSubArraySum(int numbers[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+          int start =i;
+          for(int j = i; j < numbers.length; j++) {
+            int end = j;
+            currentSum = 0;
+            for (int k = start; k <= end; k++) {
+                currentSum += numbers[k];
+            }
+            System.out.println(currentSum);
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+          }
+        }
+
+        System.out.println("Maximum Subarray Sum: " + maxSum);
        
+    }
+
+    public static void main(String[] args) {
+       int numbers[] = {1, -2, 3, 4, -5, 8};
+       maxSubArraySum(numbers);
     }
 
  
